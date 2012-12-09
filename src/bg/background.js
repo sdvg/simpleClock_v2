eb310241301a421c39c62c69018e4d22b2e4f13a
Stefan Dietz <dev@stefan-dietz.eu>
2012-12-09 21:49:21 +0100

    basic functionality that renders canvas and updates browserAction

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,9 +5,38 @@
 // });
 
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+var pad = function(i) {
+    return i >= 10 ? i : '0'+i;
+}
+
+var getFormatedDate = function(date) {
+    //todo: formats, calendar week
+    return pad(date.getDate())+'.'+pad(date.getMonth())+'.'+date.getFullYear();
+}
+
+var canvas = document.createElement('canvas');
+canvas.width = 19;
+canvas.height = 19;
+
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = '#000';
+ctx.font = '8pt Arial';
+
+
+var setTime = function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    var now = new Date();
+    var hours = pad(now.getHours());
+    var minutes = pad(now.getMinutes());
+
+    ctx.fillText(hours+':', 0, 8);
+    ctx.fillText(minutes, 7, 19);
+
+    chrome.browserAction.setIcon({'imageData': ctx.getImageData(0, 0, canvas.width, canvas.height)});
+    chrome.browserAction.setTitle({'title': getFormatedDate(now)+', '+hours+':'+minutes});
+
+    setTimeout(setTime, (60-now.getSeconds())*1000);
+}
+
+setTime();
